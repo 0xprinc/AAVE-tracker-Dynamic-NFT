3f36e24d544ebcd38048f84207f9057f4a07911d
0xprinc <0xprinc@gmail.com>
2023-07-27 17:59:57 +0530

    Update Tracker.sol

--- src/uintTracker.sol
+++ src/uintTracker.sol
@@ -2,13 +2,7 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/token/ERC721/ERC721.sol";
-import "@openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/access/Ownable.sol";
 import {Base64} from "./Base64.sol";
-
-contract Caller{
-    function get() public view returns(uint){}
-}
 
 contract Tracker is ERC721, Base64 {
 
@@ -16,7 +10,9 @@ contract Tracker is ERC721, Base64 {
 
         struct info {
         string name;                  // name that the user wants to give to their nft
-        Caller caller;                // caller contract address
+        address target;               // caller contract address
+        bytes data;                   // data to send to the target
+        uint datatype;                // 1 for uint, 2 for address, 3 for bool, 4 for string
     }
 
     constructor() ERC721("Track", "TRACK") {}
@@ -25,36 +21,41 @@ contract Tracker is ERC721, Base64 {
         super._burn(tokenId);
     }
 
-    function create2deploy(bytes memory _bytecode, uint _salt) internal returns(address callerAddress){
-        bytes memory initCode = abi.encodePacked(_bytecode);
-        assembly {
-            callerAddress :=
-                create2(0, add(initCode, 0x20), mload(initCode), _salt)
-            if iszero(extcodesize(callerAddress)) { revert(0, 0) }
-        }
-        return address(callerAddress);
-    }
     function mint(
         address to, 
         uint256 _tokenId, 
         string memory _name,
-        bytes memory _bytecode,
-        uint _salt) 
+        address _target,
+        bytes memory _data,
+        uint _datatype) 
     public {
         _safeMint(to, _tokenId);
-        setDesc(_bytecode, _salt, _tokenId, _name);
+        setDesc(_target, _data, _tokenId, _name, _datatype);
     }
 
-    function setDesc(bytes memory _bytecode, uint _salt, uint _tokenId, string memory _name) public{
+    function setDesc(address _target, bytes memory _data, uint _tokenId, string memory _name, uint _datatype) public{
         require(ownerOf(_tokenId) == msg.sender, "You are not the owner");
-        Caller c = Caller(create2deploy(_bytecode, _salt));
-        desc[_tokenId] = info(_name, c);
+        desc[_tokenId] = info(_name, _target, _data, _datatype);
     }
 
     function getSvg(uint tokenId) public view returns (string memory) {
-        uint output = (desc[tokenId].caller).get();
+        string memory output;
+        info storage description = desc[tokenId];
+        (,bytes memory data) = (description.target).staticcall(description.data);
+        if(description.datatype == 1){
+            output = uint2str(abi.decode(data, (uint)));
+        }
+        else if(description.datatype == 2){
+            output = addressToString(abi.decode(data, (address)));
+        }
+        else if(description.datatype == 3){
+            output = boolToString(abi.decode(data, (bool)));
+        }
+        else if(description.datatype == 4){
+            output = abi.decode(data,(string));
+        }
         string memory svg;
-        svg = concatenateStrings("<svg width='250' height='400' xmlns='http://www.w3.org/2000/svg'><style>text{font-family:Arial,sans-serif;font-size:24px;fill:#fff;text-anchor:middle}</style><rect width='100%' height='100%'/><text x='125' y='150' style='font-weight:700;fill:#0af'>",desc[tokenId].name,"</text><text x='125' y='250' style='font-style:italic;fill:#f50'>",uint2str(output),"</text></svg>");
+        svg = concatenateStrings("<svg width='250' height='400' xmlns='http://www.w3.org/2000/svg'><style>text{font-family:Arial,sans-serif;font-size:24px;fill:#fff;text-anchor:middle}</style><rect width='100%' height='100%'/><text x='125' y='150' style='font-weight:700;fill:#0af'>",description.name,"</text><text x='125' y='250' style='font-style:italic;fill:#f50'>",output,"</text></svg>");
         return svg;
     }    
 
